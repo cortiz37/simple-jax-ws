276755b91117711c0eb87e12a4aa053b511efdda
carlos pupo <carlos.pupo@jobsity.io>
2021-04-15 15:02:26 -0500

    wsdl fix.

--- src/main/java/com/client/WSTester.java
+++ src/main/java/com/client/WSTester.java
@@ -13,24 +13,35 @@ import java.util.Map;
 public class WSTester {
 
     public static void main(String[] args) throws Exception {
-//        ElementService elementService = new ElementService();
-//
-//        final CustomerWS servicePort = elementService.getElementServicePort();
-//
-//        requestData(servicePort);
+        ElementService elementService = new ElementService();
+        //ElementService elementService = new ElementService(new URL("file:///PATH_TO_PROJECT/simple-jax-ws/src/main/java/com/client/CustomerWS.xml"));
+
+        final CustomerWS servicePort = elementService.getElementServicePort();
+        //updatePortURL(servicePort);
+
+        requestData(servicePort);
     }
 
-//    private static void requestData(CustomerWS servicePort) {
-//        printAllElementNames(servicePort);
-//    }
-//
-//    private static void printAllElementNames(CustomerWS servicePort) {
-//
-//    }
-//
-//    private static Element buildClient() {
-//        return null;
-//    }
+    private static void requestData(CustomerWS servicePort) {
+        printAllElementNames(servicePort);
+        System.out.println("=================");
+        System.out.println(servicePort.createElement(buildClient()));
+        System.out.println("=================");
+        printAllElementNames(servicePort);
+    }
+
+    private static void printAllElementNames(CustomerWS servicePort) {
+        servicePort.getAllElements().forEach(System.out::println);
+    }
+
+    private static Element buildClient() {
+        Element element = new Element();
+        element.setDescription("from soap");
+        element.setName("from Client");
+        element.setAmount(50);
+        element.setSuccess(true);
+        return element;
+    }
 
 //    private static void setTimeout(CustomerWS ws, int seconds) {
 //        BindingProvider bindingProvider = (BindingProvider) ws;
@@ -46,11 +57,11 @@ public class WSTester {
 //        bindingProvider.getRequestContext().put(MessageContext.HTTP_REQUEST_HEADERS, headers);
 //    }
 
-//    private static void updatePortURL(CustomerWS ws) {
-//        BindingProvider bindingProvider = (BindingProvider) ws;
-//        bindingProvider.getRequestContext().put(
-//            BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-//            "http://localhost:8080/ws-app/elementService/CustomerWS"
-//        );
-//    }
+    private static void updatePortURL(CustomerWS ws) {
+        BindingProvider bindingProvider = (BindingProvider) ws;
+        bindingProvider.getRequestContext().put(
+            BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+            "http://localhost:8090/ws-app/elementService/CustomerWS"
+        );
+    }
 }
